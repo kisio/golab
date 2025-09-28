730579dcdd676e12ff8c4030c896bac62e207811
kisio <derrickkisio@gmail.com>
2025-09-28 20:15:01 +0300

    two

--- two/main.go
+++ two/main.go
@@ -3,8 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	var name string = "derrick"
+	var role string = "devops engineer"
 	fmt.Print("hello ")
 	fmt.Print("human \n")
 	fmt.Println("hello humans")
-	fmt.Println("hello humans again")
+	fmt.Println("hello humans again", name)
+
+	//formatted strings %_ is a format speciifier with _ being anything
+
+	fmt.Printf("my name is  %q and i am a %q", name, role)
+	fmt.Printf("role is of type %T \n", role)
+	fmt.Printf("you scored %0.2f points \n", 100.00)
+
+	//there are many format specifiers
+	//Sprintf(save formatted strings)
+
+	var str string = fmt.Sprintf("my name is  %q and i am a %q", name, role)
+	fmt.Println("the saved string is ", str)
 }
