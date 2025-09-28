2630c5a42a083e8fb9729c9c75e80834abb5b183
kisio <derrickkisio@gmail.com>
2025-09-28 20:00:19 +0300

    initial commit

---
+++ one/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	// //string
+	// var name string = "jane Does"
+	// fmt.Println(name)
+	// //type inference
+	// var sName = "john Smith"
+	// fmt.Println(sName)
+
+	// var lName string = "mike james"
+	// fmt.Println(name, sName, lName)
+
+	// name = "derrick"
+	// sName = "selempo"
+	// lName = "kisio"
+
+	// fmt.Println(name, sName, lName)
+
+	// //short declaration
+	// //cant be used outside of a function
+	// nName := "james bond"
+	// fmt.Println(nName)
+
+	//int
+	// var age int = 26
+	// var age2 = 30
+	// age3 := 40
+
+	// fmt.Println(age, age2, age3)
+
+	// var numOne int8 = 21
+	// var numtwo int8 = -128
+	// var numthree uint = 23
+	// fmt.Println(numOne, numtwo, numthree)
+
+	//float
+	var scoreOne float32 = -1.6
+	var scoreTwo float64 = 32434354564545
+
+	fmt.Println(scoreOne, scoreTwo)
+}
